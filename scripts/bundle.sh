0d72e12a668ab34f44077dad050f64893af6c9ea
Ryan Hirsch <ryan.hirsch@mixpanel.com>
2019-09-07 17:33:55 -0700

    add script to automate creating the publish bundle

---
+++ scripts/bundle.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR="$( cd "${SCRIPTS_DIR}" && cd .. && pwd )"
+
+if [[ -f "${PROJECT_DIR}/.env" ]]; then
+  set -o allexport
+  source "${PROJECT_DIR}/.env"
+  set +o allexport
+fi
+
+cd "${PROJECT_DIR}"
+yarn build
+cd build
+
+VERSION=$(jq -r ".version" ./manifest.json)
+
+zip -r -X "../ump-toolbox-v${VERSION}.zip" .
